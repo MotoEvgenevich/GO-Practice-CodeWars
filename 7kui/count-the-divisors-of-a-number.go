54fc816a3a491528001ac1df01a3c8ef54b796c9
Artem Kortelev <artemevgenevich89@gmail.com>
2023-05-02 15:42:38 +0300

    solve task count-the-divisors-of-a-numbers

---
+++ 7kui/count-the-divisors-of-a-number.go
@@ -0,0 +1,35 @@
+/*
+Count the number of divisors of a positive integer n.
+
+Random tests go up to n = 500000.
+
+Examples (input --> output)
+
+4 --> 3 (1, 2, 4)
+5 --> 2 (1, 5)
+12 --> 6 (1, 2, 3, 4, 6, 12)
+30 --> 8 (1, 2, 3, 5, 6, 10, 15, 30)
+Note you should only return a number, the count of divisors.
+The numbers between parentheses are shown only for you to see which numbers are counted in each case.
+*/
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(Divisors(12))
+}
+
+func Divisors(n int) int {
+	slice := []int{}
+	elements := 0
+	for i := 1; i <= n; i++ {
+		if n%i == 0 {
+			slice = append(slice, i)
+		}
+
+	}
+	elements = len(slice)
+	return elements
+}
